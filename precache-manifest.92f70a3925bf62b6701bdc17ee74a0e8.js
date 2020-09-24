f7bc0f2682ac247d6e07a8600027d93bf5294db1
Diego Koga <dikoga@gmail.com>
2020-09-24 00:48:59 -0400

    Updates

---
+++ precache-manifest.92f70a3925bf62b6701bdc17ee74a0e8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbfc739c7715393c6979f2ca2b8f2f14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bdd664cc0c0d56a5fd6",
+    "url": "/static/css/2.11b452ed.chunk.css"
+  },
+  {
+    "revision": "85caa4ba924d55675c9d",
+    "url": "/static/css/main.e806015a.chunk.css"
+  },
+  {
+    "revision": "3bdd664cc0c0d56a5fd6",
+    "url": "/static/js/2.2b0b7a8a.chunk.js"
+  },
+  {
+    "revision": "2cf5b6aece6ebb06bd9703bf7c4e7465",
+    "url": "/static/js/2.2b0b7a8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85caa4ba924d55675c9d",
+    "url": "/static/js/main.d5df8104.chunk.js"
+  },
+  {
+    "revision": "0231f95ac667b73c9187",
+    "url": "/static/js/runtime-main.91e21c86.js"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
